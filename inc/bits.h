d74dc76cffd7d7f6f8fd59fca6c1c3c329c28d29
Jos <gnu.crazier@gmail.com>
2014-11-01 15:51:13 +0800

    delete BITS_* macro which is depend on GNUC

--- inc/bits.h
+++ inc/bits.h
@@ -2,21 +2,6 @@
 
 #define BITS_PER_BYTE 8
 
-#define BITS_SET(ptr, index)					\
-({								\
-	((char *)(ptr))[(index)/8] |= (0x01 << ((index)%8));	\
-})
-
-#define BITS_CLR(ptr, index)					\
-({								\
-	((char *)(ptr))[(index)/8] &= ~(0x01 << ((index)%8));	\
-})
-
-#define BITS_TEST(ptr, index)					\
-({								\
-	(((char *)(ptr))[(index)/8] >> ((index)%8)) & 0x01;	\
-})
-
 void bits_set(void *, unsigned int);
 void bits_clr(void *, unsigned int);
 void bits_dump(void *, unsigned int);
